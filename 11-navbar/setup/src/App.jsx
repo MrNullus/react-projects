51d43bc83fd9e3d3efdf69f6dc04e0240c254afc
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-11-15 22:49:24 -0300

    changes

---
+++ 11-navbar/setup/src/App.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import Navbar from './Navbar';
+
+
+function App() {
+  return (
+    <>
+      <h2>navbar project setup</h2>
+    </>
+  );
+}
+
+export default App;
---
+++ 11-navbar/setup/src/Navbar.jsx
@@ -0,0 +1,14 @@
+import React, { useState, useRef, useEffect } from 'react';
+
+import { FaBars, FaTwitter } from 'react-icons/fa';
+import logo from './logo.svg';
+
+import { links, social } from './data';
+
+const Navbar = () => {
+  return (
+    <h4>navbar</h4>
+  );
+};
+
+export default Navbar;
