d5dcff8787c99936ae99a8094c8e3ac520ae778c
dev-gustavo-henrique <gustavojs417@gmail.com>
2022-11-20 23:28:00 -0300

    final project

--- 11-navbar/setup/src/App.jsx
+++ 11-navbar/setup/src/App.jsx
@@ -6,7 +6,9 @@ import Navbar from './Navbar';
 function App() {
   return (
     <>
-      <h2>navbar project setup</h2>
+      <Navbar />
+
+        
     </>
   );
 }
--- 11-navbar/setup/src/Navbar.jsx
+++ 11-navbar/setup/src/Navbar.jsx
@@ -5,9 +5,77 @@ import logo from './logo.svg';
 
 import { links, social } from './data';
 
+
 const Navbar = () => {
+
+  const [showLinks, setShowLinks] = useState(false);
+  const linksContainerRef = useRef(null);
+  const linksRef = useRef(null);
+
+
+  useEffect(() => {
+    const linksHeight = linksRef.current.getBoundingClientRect();
+    const height = `${showLinks ? linksHeight : '0'}px`;
+
+    linksContainerRef.current.style.height = height;
+
+    console.log('linksHeight' + linksHeight);
+    console.log('linksHeight' + linksContainerRef.current.getBoundingClientRect());
+  }, [showLinks]);
+
+
   return (
-    <h4>navbar</h4>
+    <nav>
+      <div className="nav-center">
+        <div className="nav-header">
+          <img src={logo} alt="Logo" />
+
+          <button 
+            className="nav-toggle"
+            onClick={() => setShowLinks(!showLinks)}
+          >
+            <FaBars />
+          </button>
+        </div>
+
+        {
+          showLinks && (
+            <div 
+              ref={linksContainerRef}
+              className={`${showLinks ?? 'show-container'} links-container`}
+            >
+              <ul 
+                className="links"
+                ref={linksRef}
+              >
+                {
+                  links.map(({ id, url, text }) => (
+                    <li key={ id }>
+                      <a href={ url }>
+                        { text }
+                      </a>
+                    </li>
+                  ));
+                }
+              </ul>
+            </div>  
+          )
+        }
+        
+
+        <ul className="social-icons">
+          {
+            social.map(({ id, url, icon }) => (
+              <li key={ id }>
+                <a href={ url }>
+                  { icon }
+                </a>
+              </li>
+            ));
+          }
+        </ul>
+      </div>
+    </nav>
   );
 };
 
